e765ef895fef728818d1d740746eb30b1b2a0290
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-04 02:44:22 +0430

    Created mocked data

---
+++ MusicAppAnimationChallenge/Model/Mocked Data.swift
@@ -0,0 +1,559 @@
+//
+//  Mocked Data.swift
+//  MusicAppAnimationChallenge
+//
+//  Created by Ali Aghamirbabaei on 4/4/22.
+//
+
+import Foundation
+
+// MARK: Mocked Data
+var musics: [Music] = [
+    Music(musicTitle: "Bones", artwork: "IG-Bones", lyrics: """
+[Pre-Chorus]
+My patience is waning, is this entertaining?
+Our patience is waning, is this entertaining?
+
+[Chorus]
+I-I-I, got this feeling, yeah, you know
+Where I'm losing all control
+'Cause there's magic in my bones (In my bones)
+I-I-I, got this feeling in my soul
+Go ahead and throw your stones
+'Cause there's magic in my bones
+
+[Post-Chorus]
+'Cause there's magic in my bones
+
+[Bridge]
+Look in the mirror of my mind
+Turnin' the pages of my life
+Walkin' the path so many paced a million times
+Drown out the voices in the air
+Leavin' the ones that never cared
+Pickin' the pieces up and buildin’ to the sky
+
+[Pre-Chorus]
+My patience is waning, is this entertaining?
+My patience is waning, is this entertaining?
+[Chorus]
+I-I-I, got this feeling, yeah, you know
+Where I'm losing all control
+'Cause there's magic in my bones (Magic in my bones)
+I-I-I, got this feeling in my soul (Soul)
+Go ahead and throw your stones
+'Cause there's magic in my bones
+
+[Outro]
+There goes my mind, (I-I-I) don't mind
+There goes my mind (There it goes, there it goes)
+There goes my mind, (I-I-I) don't mind
+There goes my mind (There it goes)
+'Cause there's magic in my bones
+
+Songwriters: Benjamin Arthur Mckee / Daniel Coulter Reynolds / Daniel James Platzman / Daniel Wayne Sermon / Mattias Larsson / Robin Fredriksson
+"""),
+    Music(musicTitle: "Enemy", artwork: "IG-Enemy", lyrics: """
+[Intro: Dan Reynolds]
+Look out for yourself
+
+[Verse 1: Dan Reynolds]
+I wake up to the sounds of the silence that allows
+For my mind to run around, with my ear up to the ground
+I'm searching to behold the stories that are told
+When my back is to the world that was smiling when I turned
+
+[Pre-Chorus: Dan Reynolds]
+Tell you you're the greatest
+But once you turn, they hate us
+
+[Chorus: Dan Reynolds]
+Oh, the misery
+Everybody wants to be my enemy
+Spare the sympathy
+Everybody wants to be my enemy-y-y-y-y
+(Look out for yourself)
+My enemy-y-y-y-y (Look, look, look, look)
+(Look out for yourself)
+But I'm ready
+
+[Verse 2: Dan Reynolds]
+Your words up on thе wall as you're prayin' for my fall
+And the laughter in thе halls and the names that I've been called
+I stack it in my mind, and I'm waiting for the time
+When I show you what it's like to be words spit in a mic
+
+[Pre-Chorus: Dan Reynolds]
+Tell you you're the greatest
+But once you turn, they hate us (Ha)
+
+[Chorus: Dan Reynolds & JID]
+Oh, the misery
+Everybody wants to be my enemy
+Spare the sympathy
+Everybody wants to be my enemy-y-y-y-y (Look, look, look, look)
+(Look out for yourself)
+My enemy-y-y-y-y (Look, look, look, look; yeah)
+(Look out for yourself)
+Uh, look, okay
+
+[Verse 3: JID]
+I'm hopin' that somebody pray for me
+I'm prayin' that somebody hope for me
+I'm stayin' where nobody 'posed to be p-p-posted
+Being a wreck of emotions
+Ready to go whenever, just let me know
+The road is long, so put the pedal into the floor
+The enemy on my trail, my energy unavailable
+I'ma tell 'em, "Hasta luego"
+They wanna plot on my trot to the top
+I been outta shape, thinkin' out the box, I'm an astronaut
+I blasted off the planet rock to cause catastrophe
+And it matters more because I had it not
+Had I thought about wreaking havoc
+On an opposition, kinda shockin' they wanted static
+With precision, I'm automatic quarterback
+I ain't talkin' sackin', pack it
+Pack it up, I don't panic, batter-batter up
+Who the baddest? It don't matter 'cause we at your throat
+
+
+[Chorus: Dan Reynolds]
+Everybody wants to be my enemy
+Spare the sympathy (Ah)
+Everybody wants to be my enemy
+Oh, the misery (Ah)
+Everybody wants to be my enemy
+Spare the sympathy (Ah)
+Everybody wants to be my enemy
+
+[Outro: Dan Reynolds]
+Pray it away, I swear I'll never be a saint, no way
+My enemy
+Pray it away, I swear I'll never be a saint
+(Look out for yourself)
+
+Songwriters: Benjamin Arthur Mckee / Daniel Coulter Reynolds / Daniel James Platzman / Daniel Wayne Sermon / Destin Route / Justin Tranter / Mattias Per Larsson / Robin Lennart Fredriksson
+"""),
+    Music(musicTitle: "Radioactive", artwork: "IG-Radioactive", lyrics: """
+[Intro]
+Whoah-oh
+Whoah-oh
+Whoah-oh
+Whoah
+
+[Verse 1]
+I'm waking up to ash and dust
+I wipe my brow and I sweat my rust
+I'm breathing in the chemicals
+Yeah, ah
+
+[Refrain]
+I'm breaking in, shaping up
+Then checking out on the prison bus
+This is it, the apocalypse
+Whoa
+
+[Pre-Chorus]
+I'm waking up, I feel it in my bones
+Enough to make my system blow
+Welcome to the new age, to the new age
+Welcome to the new age, to the new age
+
+
+
+[Chorus]
+Whoa-oh, whoa
+I'm radioactive, radioactive
+Whoa-oh, whoa
+I'm radioactive, radioactive
+
+[Verse 2]
+I raise my flags, dye my clothes
+It's a revolution, I suppose
+We're painted red to fit right in
+Whoa
+
+[Refrain]
+I'm breaking in, shaping up
+Then checking out on the prison bus
+This is it, the apocalypse
+Whoa
+
+[Pre-Chorus]
+I'm waking up, I feel it in my bones
+Enough to make my system blow
+Welcome to the new age, to the new age
+Welcome to the new age, to the new age
+
+[Chorus]
+Whoa-oh, whoa
+I'm radioactive, radioactive
+Whoa-oh, whoa
+I'm radioactive, radioactive
+
+
+[Bridge]
+All systems go, the sun hasn't died
+Deep in my bones, straight from inside
+
+[Pre-Chorus]
+I'm waking up, I feel it in my bones
+Enough to make my system blow
+Welcome to the new age, to the new age
+Welcome to the new age, to the new age
+
+[Chorus]
+Whoa-oh, whoa
+I'm radioactive, radioactive
+Whoa-oh, whoa
+I'm radioactive, radioactive
+
+Songwriters: Benjamin Arthur McKee / Daniel Coulter Reynolds / Daniel Wayne Sermon / Joshua Francis Mosser / Alexander Junior Grant
+"""),
+    Music(musicTitle: "Believer", artwork: "IG-Believer", lyrics: """
+[Verse 1]
+First things first
+I'ma say all the words inside my head
+I'm fired up and tired of
+The way that things have been, oh-ooh
+The way that things have been, oh-ooh
+
+[Verse 2]
+Second thing second
+Don't you tell me what you think that I could be
+I'm the one at the sail
+I'm the master of my sea, oh-ooh
+The master of my sea, oh-ooh
+
+[Pre-Chorus]
+I was broken from a young age
+Taking my sulkin' to the masses
+Writing my poems for the few
+That look at me, took to me, shook at me, feelin' me
+Singing from heartache from the pain
+Taking my message from the veins
+Speaking my lesson from the brain
+Seeing the beauty through the...
+
+
+
+[Chorus]
+Pain! You made me a, you made me a
+Believer, believer
+Pain! You break me down and build me up
+Believer, believer
+Pain! Oh, let the bullets fly, oh, let them rain
+My life, my love, my drive, it came from…
+Pain! You made me a, you made me a
+Believer, believer
+
+[Verse 3]
+Third things third
+Send a prayer to the ones up above
+All the hate that you've heard
+Has turned your spirit to a dove, oh-ooh
+Your spirit up above, oh-ooh
+
+[Pre-Chorus]
+I was chokin' in the crowd
+Building my rain up in the cloud
+Falling like ashes to the ground
+Hoping my feelings, they would drown
+But they never did, ever lived, ebbin' and flowin'
+Inhibited, limited 'til it broke open and rained down
+It rained down, like…
+
+
+[Chorus]
+Pain! You made me a, you made me a
+Believer, believer
+Pain! You break me down and build me up
+Believer, believer
+Pain! Oh, let the bullets fly, oh, let them rain
+My life, my love, my drive, it came from…
+Pain! You made me a, you made me a
+Believer, believer
+
+[Verse 4]
+Last things last
+By the grace of the fire and the flames
+You're the face of the future
+The blood in my veins, oh-ooh
+The blood in my veins, oh-ooh
+
+[Pre-Chorus]
+But they never did, ever lived, ebbin' and flowin'
+Inhibited, limited 'til it broke open and rained down
+It rained down, like…
+
+[Chorus]
+Pain! You made me a, you made me a
+Believer, believer
+Pain! You break me down and build me up
+Believer, believer
+Pain! Oh, let the bullets fly, oh, let them rain
+My life, my love, my drive, it came from…
+Pain! You made me a, you made me a
+Believer, believer
+
+Songwriters: Benjamin Arthur Mckee / Daniel Coulter Reynolds / Daniel Wayne Sermon / Mattias Per Larsson / Robin Lennart Fredriksson / Daniel James Platzman / Justin Drew Tranter
+"""),
+    Music(musicTitle: "Wrecked", artwork: "IG-Wreaked", lyrics: """
+[Verse 1]
+Days pass by, and my eyes, they dry, and I think that I'm okay
+'Til I find myself in conversation, fading away
+The way you smile, the way you walk
+The time you took to teach me all that you had taught
+Tell me, how am I supposed to move on?
+
+[Refrain]
+These days I'm becoming everything that I hate
+Wishing you were around, but now it's too late
+My mind is a place that I can't escape your ghost
+
+[Pre-Chorus]
+Sometimes I wish that I could wish it all away
+One more rainy day without you
+Sometimes I wish that I could see you one more day
+One more rainy day
+
+[Chorus]
+Oh, I'm a wreck without you here
+Yeah, I'm a wreck since you've been gone
+I've tried to put this all behind me
+I think I was wrecked all along
+Yeah, I'm a wreck
+
+[Verse 2]
+They say that the time will heal it, the pain will go away
+But everything, it reminds me of you and it comes in waves
+The way you laugh when your shoulders shook
+The time you took to teach me all that you had taught
+Tell me, how am I supposed to move on?
+
+[Refrain]
+These days I'm becoming everything that I hate
+Wishing you were around, but now it's too late
+My mind is a place that I can't escape your ghost
+
+[Pre-Chorus]
+Sometimes I wish that I could wish it all away
+One more rainy day without you
+Sometimes I wish that I could see you one more day
+One more rainy day
+
+[Chorus]
+Oh, I'm a wreck without you here
+Yeah, I'm a wreck since you've been gone
+I've tried to put this all behind me
+I think I was wrecked all along
+
+[Bridge]
+These days when I'm on the brink of the edge
+I remember the words that you said
+"Remember the life you led"
+You'd say, "Oh, suck it all up, don't get stuck in the mud
+Thinking of things that you should have done"
+I'll see you again, my loved one
+I'll see you again, my loved one
+Yeah, I'm a wreck
+I'll see you again, my loved one
+
+[Chorus]
+Yeah, I'm a wreck without you here
+(I'm a wreck without you here, loved one)
+Yeah, I'm a wreck since you've been gone
+(I'm a wreck since you've been gone)
+I've tried to put this all behind me
+I think I was wrecked all along (I'm a wreck)
+Yeah, I'm a wreck
+
+[Outro]
+Sometimes I wish that I could wish it all away (But I can't)
+One more rainy day without you (One more rainy day)
+Sometimes I wish that I could see you one more day (But I can't)
+One more rainy day
+
+Songwriters: Benjamin Arthur Mckee / Daniel Coulter Reynolds / Daniel Wayne Sermon / Daniel James Platzman
+"""),
+    Music(musicTitle: "Whatever it takes", artwork: "IG-Whatever-it-takes", lyrics: """
+[Verse 1]
+Falling too fast to prepare for this
+Tripping in the world could be dangerous
+Everybody circling is vulturous
+Negative, nepotist
+Everybody waiting for the fall of man
+Everybody praying for the end of times
+Everybody hoping they could be the one
+I was born to run, I was born for this
+
+[Pre-Chorus]
+Whip, whip
+Run me like a racehorse
+Pull me like a ripcord
+Break me down and build me up
+I wanna be the slip, slip
+Word upon your lip, lip
+Letter that you rip, rip
+Break me down and build me up
+
+[Chorus]
+Whatever it takes
+'Cause I love the adrenaline in my veins
+I do whatever it takes
+'Cause I love how it feels when I break the chains
+Whatever it takes
+Yeah, take me to the top
+I'm ready for whatever it takes
+'Cause I love the adrenaline in my veins
+I do what it takes
+
+[Verse 2]
+Always had a fear of being typical
+Looking at my body feeling miserable
+Always hanging on to the visual
+I wanna be invisible
+Looking at my years like a martyrdom
+Everybody needs to be a part of 'em
+Never be enough, I'm the prodigal son
+I was born to run, I was born for this
+
+[Pre-Chorus]
+Whip, whip
+Run me like a racehorse
+Pull me like a ripcord
+Break me down and build me up
+I wanna be the slip, slip
+Word upon your lip, lip
+Letter that you rip, rip
+Break me down and build me up
+
+[Chorus]
+Whatever it takes
+'Cause I love the adrenaline in my veins
+I do whatever it takes
+'Cause I love how it feels when I break the chains
+Whatever it takes
+Yeah, take me to the top
+I'm ready for whatever it takes
+'Cause I love the adrenaline in my veins
+I do what it takes
+
+[Bridge]
+Hypocritical, egotistical
+Don't wanna be the parenthetical, hypothetical
+Working hard on something that I'm proud of, out of the box
+An epoxy to the world and the vision we've lost
+I'm an apostrophe
+I'm just a symbol to remind you that there's more to see
+I'm just a product of the system, a catastrophe
+And yet a masterpiece, and yet I'm half-diseased
+And when I am deceased
+At least I'll go down to the grave and die happily
+And leave the body and the soul to be a part of thee
+I do what it takes
+
+[Chorus]
+Whatever it takes
+'Cause I love the adrenaline in my veins
+I do whatever it takes
+'Cause I love how it feels when I break the chains
+Whatever it takes
+Yeah, take me to the top
+I'm ready for whatever it takes
+'Cause I love the adrenaline in my veins
+I do what it takes
+
+Songwriters: Benjamin Arthur McKee / Daniel Coulter Reynolds / Daniel James Platzman / Daniel Wayne Sermon / Joel Little
+"""),
+    Music(musicTitle: "I Bet My Life", artwork: "IG-I-Bet-My-Life", lyrics: """
+[Intro]
+(I bet, I bet my life)
+(I bet, I bet my life)
+(I bet, I bet my life)
+(I bet, I bet my life)
+
+[Verse 1]
+I know I took the path
+That you would never want for me
+I know I let you down, didn't I?
+So many sleepless nights
+Where you were waiting up on me
+Well, I'm just a slave unto the night
+
+[Pre-Chorus]
+(I bet, I bet my life)
+Now remember when I told you (I)
+That's the last you'll see of me? (I)
+Remember when I broke you down to tears? (I)
+(Woo-oooo-ooh, I bet, I bet my life)
+I know I took the path
+That you would never want for me (I)
+I gave you hell through all the years (I)
+
+[Chorus]
+So I, I bet my life
+I bet my life
+I bet my life on you
+I, I bet my life
+I bet my life
+I bet my life on you
+
+[Verse 2]
+I've been around the world
+And never in my wildest dreams
+Would I come runnin' home to you
+(I bet, I bet my life)
+I've told a million lies (I)
+But now I tell a single truth (I)
+There's you in everything I do (I)
+(Woo-oooo-ooh)
+
+[Pre-Chorus]
+Now remember when I told you
+That's the last you'll see of me?
+Remember when I broke you down to tears?
+I know I took the path
+That you would never want for me
+I gave you hell through all the years
+
+[Chorus]
+So I, I bet my life
+I bet my life
+I bet my life on you
+I, I bet my life
+I bet my life
+I bet my life on you
+
+[Bridge]
+Don't tell me that I'm wrong
+I've walked that road before
+And left you on your own
+And please believe them when they say
+That it's left for yesterday
+And the records that I play
+Please forgive me for all I've done
+
+[Chorus]
+So I, I bet my life
+I bet my life
+I bet my life on you
+I, I bet my life
+I bet my life
+I bet my life on you
+
+[Outro]
+Ehe-yee-eee
+I, I bet my...
+I bet my...
+I bet my...
+O-yeee-eeah
+I bet my...
+I, I bet my life
+I bet my life
+I bet my...
+
+Songwriters: Benjamin Arthur McKee / Daniel Coulter Reynolds / Daniel James Platzman / Daniel Wayne Sermon
+"""),
+]
