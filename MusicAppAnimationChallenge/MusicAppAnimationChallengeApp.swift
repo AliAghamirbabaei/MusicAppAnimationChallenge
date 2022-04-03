b25d578d1aa55f22d378389a32caf4151764697a
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-03 22:03:26 +0430

    Initial Commit

---
+++ MusicAppAnimationChallenge/MusicAppAnimationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppAnimationChallengeApp.swift
+//  MusicAppAnimationChallenge
+//
+//  Created by Ali Aghamirbabaei on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppAnimationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
