374a54e3bae46a12ca32f23ac470b8e90538d192
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-04 05:41:21 +0430

    Added OffsetModifier

---
+++ MusicAppAnimationChallenge/View/Components/OffsetModifier.swift
@@ -0,0 +1,35 @@
+//
+//  OffsetModifier.swift
+//  MusicAppAnimationChallenge
+//
+//  Created by Ali Aghamirbabaei on 4/4/22.
+//
+
+import SwiftUI
+
+struct OffsetModifier: ViewModifier {
+    @Binding var offset: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                GeometryReader { proxy in
+                    let minY = proxy.frame(in: .named("SCROLL")).minY
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: minY)
+                        .onPreferenceChange(OffsetKey.self) { minY in
+                            self.offset = minY
+                        }
+                }
+            }
+    }
+}
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
