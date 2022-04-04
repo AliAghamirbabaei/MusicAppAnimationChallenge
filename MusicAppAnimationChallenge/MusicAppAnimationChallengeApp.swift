ac82b3772c1276f0b169b5b19404a75a40354974
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-04 05:40:12 +0430

    Change main view to Home view

--- MusicAppAnimationChallenge/MusicAppAnimationChallengeApp.swift
+++ MusicAppAnimationChallenge/MusicAppAnimationChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MusicAppAnimationChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
